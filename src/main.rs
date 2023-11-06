702a40fdbef3faaeba80dabfc1fef09c47a8ec1d
alex <ackerander@gmail.com>
2023-11-05 23:19:11 -0800

    Improved camera control.

--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ use bevy::{prelude::*, input::mouse::*, input::common_conditions::input_just_pre
 #[cfg(feature = "inspector")]
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 use toml::Value;
-// use rand::Rng;
 use std::{fs, time::Duration};
+use core::f32::consts::PI;
 
 const G: f32 = 6.6743e-11;
 const FILE: &str = "assets/bodies.toml";
+const ACTIVATION_MAX: f32 = 120.;
+const ACTIVATION_S: f32 = 0.5;
+// const ACTIVATION_B: f32 = (ACTIVATION_S / ACTIVATION_MAX).exp();
+const ACTIVATION_B: f32 = 1.00417535929111852966;
 
 #[derive(Component)]
 struct Body {
     name: String,
     mass: f32,
     vel: Vec3,
+    angular_vel: Vec3,
 }
 
 fn parse_bodies(
@@ -21,7 +26,6 @@ fn parse_bodies(
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    // let mut rng = rand::thread_rng();
     let parse_vec3 = |val: &Value| -> Option<Vec3> {
         let table = val.as_table()?;
         let get_f = |key| Some(table[key].as_float()? as f32);
@@ -37,6 +41,7 @@ fn parse_bodies(
                 name: body_cfg["name"].as_str().unwrap_or("Unnamed").to_owned(),
                 mass: body_cfg["mass"].as_float().unwrap_or(1.) as f32,
                 vel: parse_vec3(&body_cfg["velocity"]).unwrap_or(Vec3::ZERO),
+                angular_vel: parse_vec3(&body_cfg["angular_vel"]).unwrap_or(Vec3::ZERO),
             },
             PbrBundle {
                 mesh: meshes.add(shape::UVSphere{
@@ -53,6 +58,16 @@ fn parse_bodies(
     }
 }
 
+enum CamFocus {
+    Entity(Entity),
+    Point(Vec3),
+}
+#[derive(Component)]
+struct CenterCam {
+    focus: CamFocus,
+    offset: Vec3,
+}
+
 fn setup(mut commands: Commands) {
     // light
     commands.spawn(PointLightBundle {
@@ -65,10 +80,60 @@ fn setup(mut commands: Commands) {
         ..default()
     });
     // camera
-    commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(-2., 2.5, 7.5).looking_at(Vec3::ZERO, Vec3::Y),
-        ..default()
-    });
+    commands.spawn((Camera3dBundle {
+            transform: Transform::from_xyz(0., 4., 0.).looking_at(Vec3::ZERO, Vec3::Z),
+            ..default()
+        },
+        CenterCam { focus: CamFocus::Point(Vec3::ZERO), offset: Vec3::new(0., 4., 0.) }
+    ));
+}
+
+const SENSITIVITY: f32 = 1e-2;
+use bevy::render::camera::Projection::Perspective;
+fn mouse_cam(
+    buttons: Res<Input<MouseButton>>,
+    mut ev_motion: EventReader<MouseMotion>,
+    mut ev_scroll: EventReader<MouseWheel>,
+    mut cam_query: Query<(&mut Transform, &mut Projection, &mut CenterCam), With<Camera>>
+) {
+    let (mut trans, mut proj, mut center) = cam_query.get_single_mut().unwrap();
+    let mut delta = Vec2::ZERO;
+    for ev in ev_motion.read() {
+        delta += ev.delta;
+    }
+    if buttons.pressed(MouseButton::Right) {
+        match center.focus {
+            CamFocus::Entity(e) => (),
+            CamFocus::Point(p) => {
+                // let angle = Vec3::Z.angle_between(center.offset);
+                let angle = PI - trans.forward().z.acos();
+                let rot_y = Quat::from_axis_angle(trans.right(), (-SENSITIVITY * delta.y).clamp(0.05 * PI - angle, 0.95 * PI - angle));
+                let rot = Quat::from_rotation_z(-SENSITIVITY * delta.x) * rot_y;
+                center.offset = rot * center.offset;
+                trans.translation = p + center.offset;
+                trans.rotation = -rot * trans.rotation;
+            },
+        }
+    }
+    if buttons.pressed(MouseButton::Middle) {
+        if let CamFocus::Point(p) = &mut center.focus {
+            let dolly = 0.005 * (delta.x * trans.left() + delta.y * trans.up());
+            *p += dolly;
+            trans.translation += dolly;
+            println!("{}", p);
+        }
+    }
+    ev_motion.clear();
+
+    // TODO: More sophisticated zooming
+    if let Perspective(p) = proj.as_mut() {
+        for ev in ev_scroll.read() {
+            match ev.unit {
+                MouseScrollUnit::Line => p.fov = (p.fov * (1. - 0.2 * ev.y)).clamp(0.1f32.to_radians(), 180.0f32.to_radians()),
+                MouseScrollUnit::Pixel => p.fov = (p.fov * (1. - 0.2 * ev.y)).clamp(0.1f32.to_radians(), 180.0f32.to_radians()),
+            }
+        }
+    }
 }
 
 #[derive(Default, Clone)]
@@ -112,42 +177,8 @@ fn update_bodies(mut bodies: Query<(&mut Transform, &mut Body)>, delta_t: Res<Ti
 
     for ((mut transform, mut body), soln) in bodies.iter_mut().zip(&soln_vec) {
         transform.translation += soln.sum.0;
+        transform.rotation *= Quat::from_scaled_axis(dt * body.angular_vel);
         body.vel += soln.sum.1;
-    }
-}
-
-const SENSITIVITY: f32 = 0.001;
-use bevy::render::camera::Projection::Perspective;
-fn mouse_cam(
-    buttons: Res<Input<MouseButton>>,
-    mut ev_motion: EventReader<MouseMotion>,
-    mut ev_scroll: EventReader<MouseWheel>,
-    mut cam_query: Query<(&mut Transform, &mut Projection), With<Camera>>
-) {
-    let (mut trans, mut proj) = cam_query.get_single_mut().unwrap();
-    let mut delta = Vec2::ZERO;
-    for ev in ev_motion.read() {
-        delta += ev.delta;
-    }
-    if buttons.pressed(MouseButton::Right) {
-        let yaw = Quat::from_rotation_y(-SENSITIVITY * delta.x);
-        let pitch = Quat::from_rotation_x(-SENSITIVITY * delta.y);
-        trans.rotation = yaw * trans.rotation * pitch;
-    }
-    if buttons.pressed(MouseButton::Middle) {
-        let up = trans.up();
-        let left = trans.left();
-        trans.translation += 0.005 * (delta.x * left + delta.y * up);
-    }
-    ev_motion.clear();
-
-    if let Perspective(p) = proj.as_mut() {
-        for ev in ev_scroll.read() {
-            match ev.unit {
-                MouseScrollUnit::Line => p.fov = (p.fov - 0.1 * ev.y).clamp(0.1f32.to_radians(), 180.0f32.to_radians()),
-                MouseScrollUnit::Pixel => p.fov = (p.fov - 0.1 * ev.y).clamp(0.1f32.to_radians(), 180.0f32.to_radians()),
-            }
-        }
     }
 }
 
